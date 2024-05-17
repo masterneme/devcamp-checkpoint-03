32f921f25a5ec8b2ec8004a111f270cbc57c4d8f
masterneme <8470866+masterneme@users.noreply.github.com>
2024-05-17 18:03:36 +0200

    Add files via upload

---
+++ checkpoint3.py
@@ -0,0 +1,35 @@
+# Exercise 1
+string_variable = "Hola mundo! Qué tal?"
+number_variable = 12345
+list_variable = ["elemento0", "elemento1", "etc"]
+boolean_variable = True
+
+# Exercise 2
+first_letters = string_variable[0:3]
+
+# Exercise 3
+list_element = list_variable[0]
+
+# Exercise 4
+new_number = number_variable + 10
+
+# Exercise 5
+list_variable[-1]
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+name_list = names.split(',')
+
+# Exercise 7
+first_word = string_variable[0:4].upper()
+new_string = first_word + string_variable[4:]
+
+# Exercise 8
+print(f'El password {number_variable} es muy débil.')
+
+# Exercise 9
+print("hello world")
+
+# Exercise 10
+hola_index = string_variable.find("Hola")
+string_variable = string_variable.replace("Hola", "adiós")
